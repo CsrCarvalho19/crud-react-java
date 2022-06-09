636c0a52e8beb4f3ed583d40afd7ae110d6ff86d
Cesar Carvalho <cesar.carvalho19@live.com>
2022-06-09 20:52:37 -0300

    Backend base project and mysql docker-compose.

---
+++ backend-employee/src/test/java/com/backend/backendemployee/BackendEmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.backendemployee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendEmployeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
